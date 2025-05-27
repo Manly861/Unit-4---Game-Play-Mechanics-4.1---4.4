cc3f2d510d08ffc084082074627d59bbeb996157
Ly_Man <lym861@hsd.k12.or.us>
2025-05-27 13:10:06 -0700

    adding a power up object and it will be destroy when it was collided by player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,5 +18,14 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Power"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
     }
 }
